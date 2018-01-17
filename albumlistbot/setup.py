43ff02557129995bf8021bf2b23fb560df307553
Ogreman <theogreman@gmail.com>
2018-01-17 09:43:45 +0000

    Debug

--- albumlistbot/setup.py
+++ albumlistbot/setup.py
@@ -25,7 +25,7 @@ def create_app():
     app = flask.Flask(__name__)
     if 'DYNO' in os.environ:
         app.logger.addHandler(logging.StreamHandler(sys.stdout))
-        app.logger.setLevel(logging.INFO)
+        app.logger.setLevel(logging.DEBUG)
     app.config.from_object(os.environ['APP_SETTINGS'])
     add_blueprints(app)
     app.logger.info(f'[app]: created with {os.environ["APP_SETTINGS"]}')
