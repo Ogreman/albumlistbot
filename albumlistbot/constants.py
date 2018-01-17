314875150297c02708596d9c29b22c09b61e41e2
Ogreman <theogreman@gmail.com>
2018-01-17 10:27:45 +0000

    Add scope in the hope that this allows us to create apps

--- albumlistbot/constants.py
+++ albumlistbot/constants.py
@@ -2,7 +2,7 @@ URL_REGEX = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-
 HASHTAG_REGEX = '#(?:[a-zA-Z]|[0-9]|[-_.+])+'
 SLACK_CHANNEL_REGEX = '<#(C[0-9A-Z]+)\|([a-z]+)>'
 SLACK_AUTH_URL = 'https://slack.com/api/oauth.access?client_id={client_id}&client_secret={client_secret}&code={code}'
-HEROKU_AUTH_URL = 'https://id.heroku.com/oauth/authorize?client_id={client_id}&response_type=code&scope=read%20write&state={csrf_token}'
+HEROKU_AUTH_URL = 'https://id.heroku.com/oauth/authorize?client_id={client_id}&response_type=code&scope=read-protected%20write-protected&state={csrf_token}'
 HEROKU_TOKEN_URL = 'https://id.heroku.com/oauth/token'
 HEROKU_API_URL = 'https://api.heroku.com'
 HEROKU_HEADERS = {
